ec33d6b3d22f5dd9688b08ab72dcf92fa1515f2f
Tyler Porter <tyler.b.porter@gmail.com>
2020-05-26 02:11:19 -0400

    Install bootstrap for control panel

--- control_panel/app/__init__.py
+++ control_panel/app/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
-def create_app():
-    app = Flask(__name__)
-    Bootstrap(app)
-
-    return app
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 from app import routes
