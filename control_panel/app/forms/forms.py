b8c4e4b91b241be2376fb600534e4e70638acbf5
Tyler Porter <tyler.b.porter@gmail.com>
2020-05-27 21:17:01 -0400

    Add a lot of new routes and CSS for control panel

---
+++ control_panel/app/forms/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm,
+from wtforms import StringField, TextField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class BotForm(FlaskForm):
+    pass
--- control_panel/app/routes.py
+++ control_panel/app/routes.py
@@ -5,3 +5,15 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/new', methods=('GET', 'POST'))
+def create_bot():
+    return render_template('new.html')
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('errors/500.html'), 500
